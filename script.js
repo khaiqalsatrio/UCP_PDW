1f937e7651f9d14ad20f96a3eeb0ad9ed3cc3846
khaiqalsatrio <khaiqalsatrio29@gmail.com>
2024-06-08 12:58:57 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,5 @@
+function submit() {
+  const name = document.getElementById('name').value.trim();
+  const email = document.getElementById('email').value.trim();
+  const notelp = document.getElementById('notelp').value.trim();
+}
